b9d2f5ed760b6b88eb884ff2c27754949accf20b
JianqiaoHe <jianqiaohe@nwcdcloud.cn>
2022-11-24 12:18:22 +0800

    add package shell

---
+++ package.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/ bash
+
+zip scale-out-computing-on-aws.zip -r ../scale-out-computing-on-aws -x "scale-out-computing-on-aws/.git/*" "scale-out-computing-on-aws/docs/*" "scale-out-computing-on-aws/.idea/*" "scale-out-computing-on-aws/.github/*"
